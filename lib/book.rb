27f199de726f1393d19d8f05e1e0f7b0236bce03
Stellamargy <stella.oloo@student.moringaschool.com>
2023-02-10 13:26:46 +0300

    completed lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,28 @@
+class Book
+    
+    attr_accessor:title,:author, :page_count, :genre
+    def initialize (title= "And Then There Were None")
+        @title=title  
+        
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+    
+
+end
+book = Book.new("The World According to Garp")
+#puts book.title
+book.author="Agatha Christie"
+#puts book.author
+book.page_count=272
+#puts book.page_count
+book.genre="Mystery"
+#puts book.genre
+
+
+
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor:brand,:color,:size,:material,:condition
+    def initialize(brand="Adidas")
+        @brand=brand
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition="new"
+    end
+
+end
+shoe=Shoe.new("Nike")
+shoe.color="red"
+shoe.size=9.5
+shoe.material = "suede"
+shoe.condition = "old"
+puts shoe.cobble
